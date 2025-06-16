b4cf98580a854fec8ed9a0c415cb326aa2349375
Adam Khan <adammkhan47@gmail.com>
2025-06-15 19:16:01 -0500

    Pretty much finished, now for polishing

---
+++ script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    const formGrades = document.getElementById("fGrades").value;
+    const summativeGrades = document.getElementById("sGrades").value;
+    const formGradesArray = formGrades.split(",");
+    const sumGradesArray = summativeGrades.split(",");
+    const numFormGradesArray = formGradesArray.map(Number);
+    const numSumGradesArray = sumGradesArray.map(Number);
+    const formGradeCount = numFormGradesArray.length;
+    const sumGradeCount = numSumGradesArray.length;
+    let formGradeAddedUp = 0;
+    let sumGradeAddedUp = 0;
+    for (let i = 0; i<numFormGradesArray.length; i++) {
+        formGradeAddedUp += numFormGradesArray[i];
+    }
+    for (let i = 0; i<numSumGradesArray.length; i++) {
+        sumGradeAddedUp += numSumGradesArray[i];
+    }
+    const result = ((formGradeAddedUp / formGradeCount) * 0.3)+((sumGradeAddedUp / sumGradeCount) * 0.7);
+    document.getElementById("grade").innerHTML = result;
+}
